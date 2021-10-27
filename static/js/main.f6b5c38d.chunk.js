353f73b0d71ac97ae3a920249aa4bd5784d36d42
mmmG <a331998513@gmail.com>
2021-10-27 22:59:24 +0200

    Updates

--- static/js/main.f6b5c38d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptest_october=this.webpackJsonptest_october||[]).push([[0],{9:function(t,e,c){"use strict";c.r(e);var n=c(0),o=c.n(n),r=c(3),s=c.n(r),i=c(1),b=function(){return Object(i.jsx)("div",{children:"Hello"})};s.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(b,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.f6b5c38d.chunk.js.map
